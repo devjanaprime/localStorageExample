6b8e9f940c37d1c032b98b90db29fcfdd5959cea
devjanaprime <dev@primeacademy.io>
2017-07-06 10:16:56 -0500

    added basic angular

--- public/scripts/localStorage.js
+++ public/scripts/localStorage.js
@@ -1,1 +1,13 @@
-console.log( 'js' );
+var myApp = angular.module( 'myApp', [] );
+
+myApp.controller( 'LocalStorageController', function(){
+  var vm = this;
+
+  vm.getLocalStorage = function(){
+    console.log( 'in LocalStorageController getLocalStorage' );
+  }; //end setLocalStorage
+
+  vm.setLocalStorage = function(){
+    console.log( 'in LocalStorageController setLocalStorage' );
+  }; //end setLocalStorage
+});
